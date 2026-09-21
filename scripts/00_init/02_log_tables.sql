982e609eb06878bff1f140bf12c44a2f810c8de0
Toyib Otusanya <toyibotusanya@gmail.com>
2026-09-21 13:53:47 +0000

    Update 02_log_tables.sql

--- scripts/00_init/02_log_tables.sql
+++ scripts/00_init/02_log_tables.sql
@@ -66,9 +66,9 @@ CREATE TABLE etl.step_log
 	step_load_status NVARCHAR(50) NOT NULL,
 	rows_extracted INT NOT NULL,
 	rows_inserted INT NOT NULL,
-	rows_updated INT NOT NULL,
-	rows_unchanged INT NOT NULL,
-	rows_flagged INT NOT NULL,
+	rows_updated INT,
+	rows_unchanged INT,
+	rows_flagged INT,
 	CONSTRAINT fk_step_log_batch_id FOREIGN KEY (batch_id) REFERENCES etl.batch_log (batch_id),
 	CONSTRAINT chk_step_log_layer CHECK(layer IN ('Bronze', 'Silver', 'Gold')),
 	CONSTRAINT chk_step_log_step_load_status CHECK(step_load_status IN ('Running', 'Successful', 'Failed'))
